ca574686facbda5af53b8d4813c75c49291d884c
Gravis <git.devuan@adaptivetime.com>
2015-11-23 13:54:43 -0500

    added example

---
+++ example.cpp
@@ -0,0 +1,40 @@
+#include "pdtk.h"
+
+#include <iostream>
+
+class ObjectDemo : public Object
+{
+public:
+
+  ObjectDemo(void)
+  {
+    connect(test_signal, this, &ObjectDemo::activated);
+  }
+
+  void trigger(int num)
+  {
+    std::cout << "executing trigger" << std::endl;
+    queue(test_signal, num, 99);
+  }
+
+  void activated(int num, int num2)
+  {
+    std::cout << "num: " << num << std::endl;
+    std::cout << "num2: " << num2 << std::endl;
+  }
+
+  signal<int, int> test_signal;
+};
+
+int main(int argc, char* argv[])
+{
+  Application app;
+
+  ObjectDemo demo;
+  std::cout << "before trigger" << std::endl;
+  demo.trigger(7);
+  std::cout << "after trigger" << std::endl;
+
+  std::cout << "process signals" << std::endl;
+  return app.exec();
+}
--- pdtk.h
+++ pdtk.h
@@ -2,7 +2,7 @@
 #define PDTK_H
 
 #include "object.h"
-#include "socket.h"
+//#include "socket.h"
 #include "posix_helpers.h"
 
 #endif // PDTK_H
