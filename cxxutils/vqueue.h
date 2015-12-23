9ca897319bdb6f2736a3ef2a438200f01d3b8654
Gravis <git.devuan@adaptivetime.com>
2015-12-23 13:50:55 -0500

    better bounds checking

--- cxxutils/vqueue.h
+++ cxxutils/vqueue.h
@@ -60,28 +60,28 @@ public:
     m_virt_end   = m_data.get() + sz;
   }
 
-  inline bool shrink (ssize_t count)
+  inline bool shrink(ssize_t count)
   {
-    if(count)
+    if(count > 0)
     {
       if(m_virt_begin + count < m_virt_end)
         m_virt_begin += count;
       else
         m_virt_begin = m_virt_end;
     }
-    return count && m_virt_begin != m_virt_end;
+    return count > 0 && m_virt_begin != m_virt_end;
   }
 
-  inline bool expand (ssize_t count)
+  inline bool expand(ssize_t count)
   {
-    if(count)
+    if(count > 0)
     {
       if(m_virt_end + count < end())
         m_virt_end += count;
       else
         m_virt_end = end();
     }
-    return count && m_virt_end != end();
+    return count > 0 && m_virt_end != end();
   }
 
   template<typename T = char> inline const T& front   (void) const { return *data<T>(); }
