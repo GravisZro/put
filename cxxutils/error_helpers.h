22aafc86e3e092e34d941ff25b5fb324767081d6
Gravis <git.devuan@adaptivetime.com>
2015-12-24 03:15:45 -0500

    moved error helpers to own file

---
+++ cxxutils/error_helpers.h
@@ -0,0 +1,20 @@
+#ifndef ERROR_HELPERS_H
+#define ERROR_HELPERS_H
+
+// STL
+#include <system_error>
+
+template<typename T>
+constexpr bool operator ==(std::errc err, T err_num)
+  { return *reinterpret_cast<T*>(&err) == err_num; }
+
+template<typename T>
+constexpr bool operator ==(T err_num, std::errc err)
+  { return *reinterpret_cast<T*>(&err) == err_num; }
+
+
+static const int error_response = -1;
+
+
+#endif // ERROR_HELPERS_H
+
