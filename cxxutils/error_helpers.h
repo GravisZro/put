72ed9e2893ea344c29e2000695ea42b8d3676ba9
Gravis <git.devuan@adaptivetime.com>
2015-12-29 11:27:28 -0500

    add success constant

--- cxxutils/error_helpers.h
+++ cxxutils/error_helpers.h
@@ -12,7 +12,9 @@ template<typename T>
 constexpr bool operator ==(T err_num, std::errc err)
   { return *reinterpret_cast<T*>(&err) == err_num; }
 
-namespace posix {
+namespace posix
+{
+  static const int success = 0;
   static const int error_response = -1;
 }
 
