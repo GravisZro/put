9f3e447f9400262526145abb43072f3d72dd09b1
Gravis <git.devuan@adaptivetime.com>
2016-02-08 11:59:29 -0500

    overhauled to use poll() to merge multiple sockets into a single IO thread

--- asocket.cpp
+++ asocket.cpp
@@ -18,6 +18,19 @@
 
 static_assert(sizeof(uint8_t) == sizeof(char), "size mismatch!");
 
+enum : index_t
+{
+  command_index = 0,
+  socket_index,
+};
+
+namespace LocalCommand
+{
+  static const uint64_t write  = 1;
+  static const uint64_t update = 2;
+  static const uint64_t exit   = 3;
+}
+
 namespace posix
 {
   inline bool getpeercred(fd_t sockfd, proccred_t& cred)
@@ -25,195 +38,236 @@ namespace posix
 }
 
 AsyncSocket::AsyncSocket(EDomain domain, EType type, EProtocol protocol, int flags)
-  : AsyncSocket(posix::socket(domain, type, protocol, flags))
-{
-}
+  : AsyncSocket(posix::socket(domain, type, protocol, flags)) { }
 
 AsyncSocket::AsyncSocket(posix::fd_t socket)
-  : m_socket(socket)
+  : m_socket(socket),
+    m_bound(false)
 {
+  posix::fd_t cmdio[2];
+  assert(::pipe(cmdio) != posix::error_response);
+  m_read_command  = cmdio[0];
+  m_write_command = cmdio[1];
+
+  m_io.reserve(1024);
+  m_io.emplace_back(m_read_command, POLLIN, 0);
+  m_io.emplace_back(m_socket, POLLIN, 0);
 }
 
 AsyncSocket::~AsyncSocket(void)
 {
-  m_read.disconnect();
-  m_write.disconnect();
+  posix::write(m_write_command, &LocalCommand::exit, sizeof(uint64_t));
+  m_iothread.join(); // wait for thread to exit
+
+  for(auto& pos : m_io)
+    posix::close(pos.fd);
+  m_io.resize(0);
+  while(!m_expired.empty())
+    m_expired.pop();
 
   if(m_socket != posix::invalid_descriptor)
     posix::close(m_socket);
-  m_socket = posix::invalid_descriptor;
   if(m_selfaddr != EDomain::unspec)
     ::unlink(m_selfaddr.sun_path);
 }
 
 bool AsyncSocket::bind(const char *socket_path, int socket_backlog)
 {
-  if(m_read.is_connected())
+  if(!has_socket())
     return false;
 
   assert(std::strlen(socket_path) < sizeof(sockaddr_un::sun_path));
   m_selfaddr = socket_path;
   m_selfaddr = EDomain::unix;
-  bool ok = posix::bind(m_socket, m_selfaddr, m_selfaddr.size());
-  ok = ok && posix::listen(m_socket, socket_backlog);
-  if(ok)
-  {
-    m_accept = std::thread(&AsyncSocket::async_accept, this);
-    m_accept.detach(); // exits after AsyncSocket threads disconnect
-  }
-  return ok;
-}
-
-void AsyncSocket::async_accept(void)
-{
-  posix::sockaddr_t m_peeraddr;
-  proccred_t m_peercred;
-
-  socklen_t addrlen;
-  m_read.connection = posix::accept(m_socket, m_peeraddr, &addrlen);
-  m_write.connection = ::dup(m_read.connection);
-  if(m_read.is_connected() &&
-     posix::getpeercred(m_read.connection, m_peercred))
-  {
-    enqueue<posix::sockaddr_t&, proccred_t&>(connectedToPeer, m_peeraddr, m_peercred);
-    m_read .thread = std::thread(&AsyncSocket::async_read , this);
-    m_write.thread = std::thread(&AsyncSocket::async_write, this);
-  }
+  return m_bound = posix::bind(m_socket, m_selfaddr, m_selfaddr.size()) &&
+                   posix::listen(m_socket, socket_backlog) &&
+                   async_spawn();
 }
 
 bool AsyncSocket::connect(const char *socket_path)
 {
-  if(m_read.is_connected())
+  if(!has_socket())
     return false;
 
   assert(std::strlen(socket_path) < sizeof(sockaddr_un::sun_path));
-  proccred_t m_peercred;
-  posix::sockaddr_t m_peeraddr;
-  m_peeraddr = socket_path;
-  m_peeraddr = EDomain::unix;
+  posix::sockaddr_t peeraddr;
+  proccred_t peercred;
+  index_t index = socket_index;
+
+  peeraddr = socket_path;
+  peeraddr = EDomain::unix;
   m_selfaddr = EDomain::unspec;
 
-  m_read.connection = m_socket;
-  bool ok = m_read.connect(m_peeraddr);
-  ok = ok && posix::getpeercred(m_read.connection, m_peercred);
-  if(ok)
-  {
-    m_write.connection = ::dup(m_read.connection);
-    enqueue<posix::sockaddr_t&, proccred_t&>(connectedToPeer, m_peeraddr, m_peercred);
-    m_read .thread = std::thread(&AsyncSocket::async_read , this);
-    m_write.thread = std::thread(&AsyncSocket::async_write, this);
-  }
-  else
-    m_read.connection = posix::invalid_descriptor;
-  return ok;
+  return posix::connect(m_socket, peeraddr, peeraddr.size()) &&
+         posix::getpeercred(m_socket, peercred) &&
+         async_spawn() &&
+         Object::enqueue(connectedToPeer, index, peeraddr, peercred);
 }
 
-void AsyncSocket::async_read(void)
+
+bool AsyncSocket::async_spawn(void)
 {
-  std::mutex m;
+  if(m_iothread.joinable())
+    return posix::write(m_write_command, &LocalCommand::update, sizeof(uint64_t)) != posix::error_response;
+
+  m_iothread = std::thread(&AsyncSocket::async_io , this);
+  return m_iothread.joinable();
+}
+
+void AsyncSocket::async_io(void)
+{
+  const pollfd& local_command = m_io.at(command_index);
+  const pollfd& master_socket = m_io.at(socket_index);
   msghdr msg = {};
   iovec iov = {};
   char aux_buffer[CMSG_SPACE(sizeof(int))] = { 0 };
+  uint64_t command_buffer = 0;
+  ssize_t byte_count = 0;
 
   msg.msg_iov = &iov;
   msg.msg_iovlen = 1;
   msg.msg_control = aux_buffer;
 
-
-  int bcount = 0;
   for(;;)
   {
-    m_read.buffer.allocate(); // allocate 64KB buffer
-    std::unique_lock<std::mutex> lk(m);
-    m_read.condition.wait(lk, [this] { return m_read.is_connected() || m_read.terminate; } );
-    if(m_read.terminate)
-      return;
+    int event_count = posix::poll(m_io.data(), m_io.size());
 
-    iov.iov_base = m_read.buffer.data();
-    iov.iov_len = m_read.buffer.capacity();
-    msg.msg_controllen = sizeof(aux_buffer);
-
-    bcount = posix::recvmsg(m_read.connection, &msg, 0);
-    if(bcount == posix::error_response)
-      std::cout << std::red << "recvmsg error: " << ::strerror(errno) << std::none << std::endl << std::flush;
-    else if(!bcount)
-      m_read.connection = posix::invalid_descriptor; // connection severed!
-    else if(m_read.buffer.expand(bcount))
+    if(local_command.revents & POLLIN)
     {
-      m_read.fd = posix::invalid_descriptor;
-      if(msg.msg_controllen == CMSG_SPACE(sizeof(int)))
+      --event_count;
+      command_buffer = 0;
+      byte_count = posix::read(local_command.fd, &command_buffer, sizeof(uint64_t));
+      if(byte_count == posix::error_response)
+        continue;
+
+      std::cout << "local command: " << command_buffer << std::endl << std::flush;
+
+      switch(command_buffer)
       {
-        cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
-        if(cmsg->cmsg_level == SOL_SOCKET &&
-           cmsg->cmsg_type == SCM_RIGHTS &&
-           cmsg->cmsg_len == CMSG_LEN(sizeof(int)))
-         m_read.fd = *reinterpret_cast<int*>(CMSG_DATA(cmsg));
+        case LocalCommand::update: continue; // m_fds size changed
+        case LocalCommand::exit:
+          std::cout << "thread exiting!" << std::endl << std::flush;
+          return; // thread exit
+        case LocalCommand::write: // m_messages isn't empty
+        {
+          for(const auto& pos : m_messages)
+          {
+            index_t index = pos.index;
+            msg.msg_iov = &iov;
+            msg.msg_iovlen = 1;
+            msg.msg_control = aux_buffer;
+
+            iov.iov_base = pos.buffer.begin();
+            iov.iov_len = pos.buffer.size();
+
+            msg.msg_controllen = 0;
+
+            if(pos.fd_buffer != posix::invalid_descriptor)
+            {
+              msg.msg_controllen = CMSG_SPACE(sizeof(int));
+              cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
+              cmsg->cmsg_level = SOL_SOCKET;
+              cmsg->cmsg_type = SCM_RIGHTS;
+              cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+              *reinterpret_cast<int*>(CMSG_DATA(cmsg)) = pos.fd_buffer;
+            }
+
+            byte_count = posix::sendmsg(m_io.at(pos.index).fd, &msg, 0);
+
+            if(byte_count == posix::error_response) // error
+              std::cout << std::flush << std::endl << std::red << "sendmsg error: " << ::strerror(errno) << std::none << std::endl << std::flush;
+            else
+              Object::enqueue(writeFinished, index, byte_count);
+          }
+          m_messages.clear();
+        }
       }
-      else if(msg.msg_flags)
-        std::cout << std::red << "error, message flags: " << std::hex << msg.msg_flags << std::dec << std::none << std::endl << std::flush;
-      enqueue<vqueue&, posix::fd_t>(readFinished, m_read.buffer, m_read.fd);
     }
-  }
-}
 
-void AsyncSocket::async_write(void)
-{
-  std::mutex m;
-  msghdr msg = {};
-  iovec iov = {};
-  char aux_buffer[CMSG_SPACE(sizeof(int))] = { 0 };
-
-  msg.msg_iov = &iov;
-  msg.msg_iovlen = 1;
-  msg.msg_control = aux_buffer;
-
-  for(;;)
-  {
-    std::unique_lock<std::mutex> lk(m);
-    m_write.condition.wait(lk, [this] { return (m_write.is_connected() && !m_write.buffer.empty()) || m_write.terminate; } );
-    if(m_write.terminate)
-      return;
-
-    iov.iov_base = m_write.buffer.begin();
-    iov.iov_len = m_write.buffer.size();
-
-    msg.msg_controllen = 0;
-
-    if(m_write.fd != posix::invalid_descriptor)
+    if(event_count && m_bound && master_socket.revents & POLLIN)
     {
-      msg.msg_controllen = CMSG_SPACE(sizeof(int));
-      cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
-      cmsg->cmsg_level = SOL_SOCKET;
-      cmsg->cmsg_type = SCM_RIGHTS;
-      cmsg->cmsg_len = CMSG_LEN(sizeof(int));
-      *reinterpret_cast<int*>(CMSG_DATA(cmsg)) = m_write.fd;
-      m_write.fd = posix::invalid_descriptor;
+      --event_count;
+      posix::sockaddr_t peeraddr;
+      socklen_t addrlen = 0;
+      posix::fd_t newfd = posix::accept(m_socket, peeraddr, &addrlen);
+      if(newfd == posix::error_response)
+        std::cout << "accept error: " << ::strerror(errno) << std::endl << std::flush;
+      else
+      {
+        proccred_t peercred;
+        index_t index = add(newfd);
+        pollfd_t& connection = m_io.at(index);
+        posix::getpeercred(connection.fd, peercred);
+        enqueue(connectedToPeer, index, peeraddr, peercred);
+      }
     }
 
-    int count = posix::sendmsg(m_write.connection, &msg, 0);
-    if(count == posix::error_response) // error
-      std::cout << std::flush << std::endl << std::red << "sendmsg error: " << ::strerror(errno) << std::none << std::endl << std::flush;
-    else
-      enqueue(writeFinished, count);
-    m_write.buffer.resize(0);
+    if(event_count)
+    {
+      index_t index = m_bound ? socket_index + 1 : command_index + 1;
+      for(auto pos = m_io.begin() + index; event_count > 0 && pos != m_io.end(); ++pos, ++index)
+      {
+        if(pos->revents & (POLLHUP | POLLERR | POLLNVAL)) // connection error or connection severed
+        {
+          --event_count;
+          posix::close(pos->fd); // close connection if open
+          m_io.at(index).fd = posix::invalid_descriptor;
+          m_expired.push(index);
+        }
+        else if(pos->revents & POLLIN) // read
+        {
+          --event_count;
+          m_incomming.index = index;
+          m_incomming.buffer.allocate(); // allocate 64KB buffer
+
+          iov.iov_base = m_incomming.buffer.data();
+          iov.iov_len = m_incomming.buffer.capacity();
+          msg.msg_controllen = sizeof(aux_buffer);
+
+          byte_count = posix::recvmsg(pos->fd, &msg, 0);
+
+          if(byte_count == posix::error_response)
+            std::cout << std::red << "recvmsg error: " << ::strerror(errno) << std::none << std::endl << std::flush;
+          else if(!byte_count)
+          {
+            if(is_connected(index) &&
+               (posix::close(pos->fd) || errno == std::errc::bad_file_descriptor))
+            {
+              m_io.at(index).fd = posix::invalid_descriptor;
+              m_expired.push(index); // connection severed!
+            }
+          }
+          else if(m_incomming.buffer.expand(byte_count))
+          {
+            m_incomming.fd_buffer = posix::invalid_descriptor;
+            if(msg.msg_controllen == CMSG_SPACE(sizeof(int)))
+            {
+              cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
+              if(cmsg->cmsg_level == SOL_SOCKET &&
+                 cmsg->cmsg_type == SCM_RIGHTS &&
+                 cmsg->cmsg_len == CMSG_LEN(sizeof(int)))
+               m_incomming.fd_buffer = *reinterpret_cast<int*>(CMSG_DATA(cmsg));
+            }
+            else if(msg.msg_flags)
+              std::cout << std::red << "error, message flags: " << std::hex << msg.msg_flags << std::dec << std::none << std::endl << std::flush;
+            enqueue(readFinished, index, m_incomming);
+          }
+        }
+        else if(pos->revents)
+        {
+          --event_count;
+          std::cout << "unknown event at: " << index << " value: " << std::hex << (int)pos->revents << std::dec << std::endl << std::flush;
+        }
+      }
+    }
   }
 }
 
-bool AsyncSocket::read(void)
+bool AsyncSocket::write(message_t msg)
 {
-  if(!m_read.is_connected())
+  if(!is_connected(msg.index))
     return false;
-  m_read.condition.notify_one();
-  return true;
-}
-
-bool AsyncSocket::write(vqueue& buffer, posix::fd_t fd)
-{
-  if(!m_read.is_connected())
-    return false;
-
-  m_write.fd = fd;
-  m_write.buffer = buffer; // share buffer memory
-  m_write.condition.notify_one();
+  m_messages.push_back(msg);
+  posix::write(m_write_command, &LocalCommand::write, sizeof(uint64_t));
   return true;
 }
--- asocket.h
+++ asocket.h
@@ -3,15 +3,39 @@
 
 // STL
 #include <vector>
+#include <queue>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+
+// POSIX
+#include <poll.h>
 
 // PDTK
 #include <object.h>
 #include <cxxutils/socket_helpers.h>
 #include <cxxutils/vqueue.h>
 #include <specialized/getpeercred.h>
+
+typedef size_t index_t;
+
+struct message_t
+{
+  index_t     index;
+  vqueue      buffer;
+  posix::fd_t fd_buffer;
+};
+
+struct pollfd_t : pollfd
+{
+  pollfd_t(void) : pollfd_t(0,0,0) { }
+  pollfd_t(int fd, short int events, short int revents)
+  {
+    pollfd::fd = fd;
+    pollfd::events = events;
+    pollfd::revents = revents;
+  }
+};
 
 class AsyncSocket : public Object
 {
@@ -27,59 +51,108 @@ public:
   bool bind(const char *socket_path, int socket_backlog = SOMAXCONN);
   bool connect(const char *socket_path);
 
-  bool read(void);
-  inline bool write(posix::fd_t fd) { vqueue b(1); b.resize(1); return write(b, fd); }
-  bool write(vqueue& buffer, posix::fd_t fd = posix::invalid_descriptor);
+  bool write(message_t msg);
 
-  signal<vqueue&, posix::fd_t> readFinished;  // msesage received
-  signal<int>                  writeFinished; // message sent
-  signal<posix::sockaddr_t&, proccred_t&> connectedToPeer; // connection is open with peer
-
-protected:
-  void async_read (void);
-  void async_write(void);
-  void async_accept(void);
+  signal<index_t, message_t>                     readFinished;          // msesage received
+  signal<index_t, ssize_t>                       writeFinished;         // message sent
+  signal<index_t, posix::sockaddr_t, proccred_t> connectedToPeer;       // connection is open with peer
+  signal<index_t>                                disconnectedFromPeer;  // connection with peer was severed
 
 protected:
-  struct async_channel_t
+  void async_io    (void);
+  bool async_spawn (void);
+
+private:
+  constexpr bool has_socket()
+    { return m_socket != posix::invalid_descriptor; }
+  constexpr bool is_bound()
+    { return has_socket() && m_bound; }
+
+  constexpr bool is_connected(index_t index)
   {
-    inline async_channel_t(void)
-      : buffer(0),
-        connection(posix::invalid_descriptor),
-        fd(posix::invalid_descriptor),
-        terminate(false) { }
+    return has_socket() &&
+           m_io.size() > index &&
+           m_io.at(index).fd != posix::invalid_descriptor;
+  }
 
-    inline bool is_connected(void) const
-      { return connection != posix::invalid_descriptor; }
-
-    inline bool connect(const posix::sockaddr_t& addr)
-      { return posix::connect(connection, addr, addr.size()); }
-
-    inline void disconnect(void)
+  constexpr index_t add(posix::fd_t fd)
+  {
+    index_t pos = m_io.size();
+    if(m_expired.empty())
+      m_io.emplace_back(fd, POLLIN, 0);
+    else
     {
-      if(is_connected())
-      {
-        posix::close(connection);
-        connection = posix::invalid_descriptor;
-        terminate = true;
-        condition.notify_one();
-        thread.join();
-      }
+      pos = m_expired.front();
+      m_expired.pop();
+      m_io.at(pos) = { fd, POLLIN, 0 };
     }
+    return pos;
+  }
 
-    vqueue                  buffer;
-    posix::fd_t             connection;
-    posix::fd_t             fd;
-    std::thread             thread;
-    std::condition_variable condition;
-    bool terminate;
-  };
-
+private:
   posix::fd_t       m_socket;
+  bool              m_bound;
   posix::sockaddr_t m_selfaddr;
   std::thread       m_accept;
-  async_channel_t   m_read;
-  async_channel_t   m_write;
+  std::thread       m_iothread;
+
+  posix::fd_t m_write_command;
+  posix::fd_t m_read_command;
+  std::vector<pollfd_t> m_io;
+  std::queue<nfds_t> m_expired;
+
+  std::vector<message_t> m_messages;
+  message_t m_incomming;
+};
+
+class SingleSocket : public AsyncSocket
+{
+public:
+  template<typename... Args>
+  inline SingleSocket(Args... args) : AsyncSocket(args...), m_index(0) { init(); }
+
+  inline bool write(vqueue buffer, posix::fd_t fd_buffer = posix::invalid_descriptor)
+    { assert(m_index); return AsyncSocket::write({ m_index, buffer, fd_buffer }); }
+
+  signal<message_t>                     readFinished;          // msesage received
+  signal<ssize_t>                       writeFinished;         // message sent
+  signal<posix::sockaddr_t, proccred_t> connectedToPeer;       // connection is open with peer
+  signal<>                              disconnectedFromPeer;  // connection with peer was severed
+private:
+  inline void init(void)
+  {
+    Object::connect(AsyncSocket::readFinished, this, &SingleSocket::receive);
+    Object::connect(AsyncSocket::writeFinished, this, &SingleSocket::sent);
+    Object::connect(AsyncSocket::connectedToPeer, this, &SingleSocket::connected);
+    Object::connect(AsyncSocket::disconnectedFromPeer, this, &SingleSocket::disconnected);
+  }
+
+  inline void sent(index_t index, ssize_t count)
+  {
+    assert(m_index == index);
+    Object::enqueue(SingleSocket::writeFinished, count);
+  }
+
+  inline void receive(index_t index, message_t msg)
+  {
+    assert(m_index == index);
+    Object::enqueue(SingleSocket::readFinished, msg);
+  }
+
+  inline void connected(index_t index, posix::sockaddr_t addr, proccred_t cred)
+  {
+    m_index = index;
+    Object::enqueue(SingleSocket::connectedToPeer, addr, cred);
+  }
+
+  inline void disconnected(index_t index)
+  {
+    assert(m_index == index);
+    Object::enqueue(SingleSocket::disconnectedFromPeer);
+  }
+
+private:
+  index_t m_index;
 };
 
 #endif // ASYNCSOCKET_H
