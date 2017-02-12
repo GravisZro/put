55783cfdb8244b34f52373337208644efa2802ed
Gravis <github@adaptivetime.com>
2017-02-12 15:36:35 -0500

    basic SDL integration

---
+++ integration/sdl.cpp
@@ -0,0 +1,174 @@
+#include "sdl.h"
+
+#include <cassert>
+#include <chrono>
+#include <thread>
+#include <map>
+
+#ifdef SDL_DEBUG
+#include <iostream>
+#endif
+
+#define ASSERT assert
+
+static std::thread sdl_thread;
+static std::map<SDL_Window*, SDL::Window*> window_lookup;
+
+SDL::Window* sdl_window_lookup(uint32_t id)
+{
+  SDL_Window* ptr = SDL_GetWindowFromID(id);
+  if(ptr == nullptr)
+    return nullptr;
+  auto pos = window_lookup.find(ptr);
+  if(pos == window_lookup.end())
+    return nullptr;
+  return pos->second;
+}
+
+int sdl_event_filter(SDL::Events* obj, SDL_Event* event) noexcept
+{
+  SDL::Window* win = nullptr;
+  switch(event->type)
+  {
+    case SDL_AUDIODEVICEADDED:
+      Object::enqueue(obj->AudioDeviceAdded,
+                      *reinterpret_cast<uint32_t*>(&event->adevice.which),
+                      *reinterpret_cast<SDL::audio_device_t*>(&event->adevice.iscapture));
+      break;
+    case SDL_AUDIODEVICEREMOVED:
+      Object::enqueue(obj->AudioDeviceRemoved,
+                      *reinterpret_cast<uint32_t*>(&event->adevice.which),
+                      *reinterpret_cast<SDL::audio_device_t*>(&event->adevice.iscapture));
+      break;
+// CONTROLLER
+    case SDL_CONTROLLERDEVICEADDED:
+      Object::enqueue(obj->ControllerDeviceAdded,
+                   event->cdevice.which);
+      break;
+    case SDL_CONTROLLERDEVICEREMOVED:
+      Object::enqueue(obj->ControllerDeviceRemoved,
+                   event->cdevice.which);
+      break;
+    case SDL_CONTROLLERDEVICEREMAPPED:
+      Object::enqueue(obj->ControllerDeviceRemapped,
+                   event->cdevice.which);
+      break;
+
+    case SDL_CONTROLLERAXISMOTION:
+      Object::enqueue(obj->ControllerAxisMotion,
+                      event->caxis.which,
+                      *reinterpret_cast<uint8_t*>(&event->caxis.axis),
+                      *reinterpret_cast<int16_t*>(&event->caxis.value));
+      break;
+
+    case SDL_CONTROLLERBUTTONDOWN:
+      Object::enqueue(obj->ControllerButtonPressed,
+                      event->cbutton.which,
+                      *reinterpret_cast<SDL::Button*>(&event->cbutton.button));
+      break;
+
+    case SDL_CONTROLLERBUTTONUP:
+      Object::enqueue(obj->ControllerButtonReleased,
+                      event->cbutton.which,
+                      *reinterpret_cast<SDL::Button*>(&event->cbutton.button));
+      break;
+// JOYSTICK
+
+    case SDL_JOYDEVICEADDED:
+      Object::enqueue(obj->JoystickDeviceAdded,
+                   event->jdevice.which);
+      break;
+    case SDL_JOYDEVICEREMOVED:
+      Object::enqueue(obj->JoystickDeviceRemoved,
+                   event->jdevice.which);
+      break;
+
+    case SDL_JOYAXISMOTION:
+      Object::enqueue(obj->JoystickAxisMotion,
+                      event->jaxis.which,
+                      *reinterpret_cast<uint8_t*>(&event->jaxis.axis),
+                      *reinterpret_cast<int16_t*>(&event->jaxis.value));
+      break;
+
+    case SDL_JOYBUTTONDOWN:
+      Object::enqueue(obj->JoystickButtonPressed,
+                      event->jbutton.which,
+                      *reinterpret_cast<SDL::Button*>(&event->jbutton.button));
+      break;
+
+    case SDL_JOYBUTTONUP:
+      Object::enqueue(obj->JoystickButtonReleased,
+                      event->jbutton.which,
+                      *reinterpret_cast<SDL::Button*>(&event->jbutton.button));
+      break;
+
+// KEYBOARD
+    case SDL_KEYDOWN:
+      win = sdl_window_lookup(event->key.windowID);
+      if(win != nullptr)
+        Object::enqueue(win->KeyPressed, event->key.keysym);
+      Object::enqueue(obj->KeyPressed, event->key.windowID, event->key.keysym);
+      break;
+
+    case SDL_KEYUP:
+      win = sdl_window_lookup(event->key.windowID);
+      if(win != nullptr)
+        Object::enqueue(win->KeyReleased, event->key.keysym);
+      Object::enqueue(obj->KeyReleased, event->key.windowID, event->key.keysym);
+      break;
+
+    default:
+#ifdef SDL_DEBUG
+      std::cout << "event type: " << std::hex << event->type << std::endl << std::flush;
+#endif
+      break;
+  }
+  return 0;
+}
+
+namespace SDL
+{
+  Events::Events(void) noexcept
+  {
+    if(!sdl_thread.joinable()) // if thread already active
+      sdl_thread = std::thread(&Events::event_thread , this); // create a new thread
+  }
+
+  Events::~Events(void) noexcept
+  {
+    SDL_Event sdlevent;
+    sdlevent.type = SDL_QUIT;
+    SDL_PushEvent(&sdlevent); // trick SDL into thinking it's quiting
+    sdl_thread.detach(); // detach because thread will self terminate
+  }
+
+  void Events::event_thread(void) noexcept
+  {
+    SDL_SetMainReady();
+    ASSERT(SDL_Init(SDL_INIT_EVERYTHING) == 0);
+    SDL_SetEventFilter(reinterpret_cast<SDL_EventFilter>(sdl_event_filter), static_cast<void*>(this));
+    while(SDL_WaitEvent(nullptr) == 0); // run until there is an error
+  }
+
+  Window::Window(const char* title,
+                 int width,
+                 int height,
+                 int pos_x,
+                 int pos_y,
+                 SDL_WindowFlags flags) noexcept
+  {
+    while(!(SDL_WasInit(0) & SDL_INIT_VIDEO))
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    m_pWindow = SDL_CreateWindow(title, pos_x, pos_y, width, height, flags);
+
+    window_lookup.emplace(m_pWindow, this);
+  }
+
+  Window::~Window(void) noexcept
+  {
+    window_lookup.erase(m_pWindow);
+    SDL_DestroyWindow(m_pWindow);
+    m_pWindow = nullptr;
+  }
+
+}
---
+++ integration/sdl.h
@@ -0,0 +1,100 @@
+#ifndef PDTK_SDL_H
+#define PDTK_SDL_H
+
+#include <SDL2/SDL.h>
+#include <sys/types.h>
+
+#include <object.h>
+
+static_assert(sizeof(SDL_EventType) == sizeof(uint32_t), "bad size");
+
+namespace SDL
+{
+  enum class Button : uint8_t
+  {
+    A = SDL_CONTROLLER_BUTTON_A,
+    B,
+    X,
+    Y,
+    Back,
+    Guide,
+    Start,
+    LeftStick,
+    RightStick,
+    LeftShoulder,
+    RightShoulder,
+    DpadUp,
+    DpadDown,
+    DpadLeft,
+    DpadRight,
+  };
+
+  enum audio_device_t : uint8_t
+  {
+    output = 0,
+    capture
+  };
+
+  typedef uint32_t windowid_t;
+
+  class Events : public Object
+  {
+  public:
+    Events(void) noexcept;
+   ~Events(void) noexcept;
+
+    signal<uint32_t, audio_device_t> AudioDeviceAdded; // device index, device type
+    signal<uint32_t, audio_device_t> AudioDeviceRemoved; // device index, device type
+
+    signal<SDL_JoystickID> ControllerDeviceAdded; // joystick id
+    signal<SDL_JoystickID> ControllerDeviceRemoved; // joystick id
+    signal<SDL_JoystickID> ControllerDeviceRemapped; // joystick id
+
+    signal<SDL_JoystickID, uint8_t, int16_t> ControllerAxisMotion;   // joystick id, axis, value
+
+    signal<SDL_JoystickID, Button> ControllerButtonPressed; // joystick id, button number
+    signal<SDL_JoystickID, Button> ControllerButtonReleased; // joystick id, button number
+
+
+
+    signal<SDL_JoystickID> JoystickDeviceAdded; // joystick id
+    signal<SDL_JoystickID> JoystickDeviceRemoved; // joystick id
+
+    signal<SDL_JoystickID, uint8_t, int16_t> JoystickAxisMotion;   // joystick id, axis, value
+
+    signal<SDL_JoystickID, Button> JoystickButtonPressed; // joystick id, button number
+    signal<SDL_JoystickID, Button> JoystickButtonReleased; // joystick id, button number
+
+
+
+    signal<windowid_t, SDL_Keysym> KeyPressed; // window id, key identifier
+    signal<windowid_t, SDL_Keysym> KeyReleased; // window id, key identifier
+
+  private:
+    void event_thread(void) noexcept;
+  };
+
+  class Window : public Object
+  {
+  public:
+    Window(const char* title,
+           int width,
+           int height,
+           int pos_x = SDL_WINDOWPOS_UNDEFINED,
+           int pos_y = SDL_WINDOWPOS_UNDEFINED,
+           SDL_WindowFlags flags = SDL_WINDOW_RESIZABLE) noexcept;
+   ~Window(void) noexcept;
+
+
+    windowid_t id(void) const { return SDL_GetWindowID(m_pWindow); }
+
+    signal<SDL_Keysym> KeyPressed; // key identifier
+    signal<SDL_Keysym> KeyReleased; // key identifier
+
+  private:
+    SDL_Window* m_pWindow;
+  };
+}
+
+
+#endif // PDTK_SDL_H
