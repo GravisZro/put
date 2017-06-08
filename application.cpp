90d78d92dff45f63bd7474e4780588efc2d1853b
Gravis <github@adaptivetime.com>
2017-06-07 21:28:23 -0400

    documentation

--- application.cpp
+++ application.cpp
@@ -53,20 +53,20 @@ int Application::exec(void) noexcept // non-static function to ensure an instanc
     EventBackend::getevents();
     for(auto pos : EventBackend::results)
     {
-      if(pos.first == s_pipeio[Read])
+      if(pos.first == s_pipeio[Read]) // if this was object enqueue FD
       {
-        while(::ioctl(pos.first, I_FLUSH, FLUSHRW) == posix::error_response && // not interested in the data
-              errno == std::errc::interrupted); // ignore interruptions
-        run();
+        while(::ioctl(pos.first, I_FLUSH, FLUSHRW) == posix::error_response && // discard the data (it's merely a trigger)
+              errno == std::errc::interrupted); // don't be interrupted while flushing the FD channel
+        run(); // execute queue of object signal calls
       }
       else
       {
-        auto entries = ms_fd_signals.equal_range(pos.first);
-        for_each(entries.first, entries.second,
-          [pos](auto& entry)
+        auto entries = ms_fd_signals.equal_range(pos.first); // get all then entries for that FD
+        for_each(entries.first, entries.second, // for each FD
+          [pos](auto& fdsigpair)
         {
-          if(entry.second.first & pos.second)
-            entry.second.second(pos.first, pos.second);
+          if(fdsigpair.second.first & pos.second) // test to see if the current FD signal pair matches the triggering EventFlag
+            fdsigpair.second.second(pos.first, pos.second); // call the fuction with the FD and triggering EventFlag
         });
       }
     }
@@ -83,10 +83,10 @@ void Application::run(void) noexcept
     exec_queue.swap(ms_signal_queue); // swap the queues to prevent race conditions and the need for constant locking/unlocking
     ms_signal_queue.unlock(); // access is no longer needed
 
-    while(s_run && !exec_queue.empty()) // if haven't quit and still have signals to execute
+    while(s_run && !exec_queue.empty()) // if haven't quit and still have object signals to execute
     {
-      exec_queue.front()(); // execute current signal
-      exec_queue.pop(); // discard current signal
+      exec_queue.front()(); // execute current object signal
+      exec_queue.pop(); // discard current object signal
     }
   }
 }
