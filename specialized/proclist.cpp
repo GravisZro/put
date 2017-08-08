bed59db33ed6b8972fdc7a6790f1ec9d2fc6fb5b
Gravis <github@adaptivetime.com>
2017-08-08 17:57:20 -0400

    bsd/osx fixes

--- specialized/proclist.cpp
+++ specialized/proclist.cpp
@@ -41,23 +41,26 @@ int proclist(pid_t* list, size_t max_length)
 #include <sys/sysctl.h>
 #include <vector>
 
+// PDTK
+#include <cxxutils/misc_helpers.h>
+
 int proclist(pid_t* list, size_t max_length)
 {
   size_t length = 0;
   std::vector<struct kinfo_proc> proc_list;
-  static const int mib[] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
+  int request[4] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
 
   std::memset(list, 0, max_length);
 
-  if(::sysctl(mib, 4, nullptr, &length, nullptr, 0) != posix::success_response)
+  if(::sysctl(request, arraylength(request), nullptr, &length, nullptr, 0) != posix::success_response)
     return posix::error_response;
 
-  if(lenth > max_length)
+  if(length > max_length)
     return posix::error(std::errc::not_enough_memory);
 
   proc_list.resize(length + 10); // allowing 10 extra processes in case of surge
 
-  if(::sysctl(mib, 4, proc_list.data(), &length, nullptr, 0) != posix::success_response)
+  if(::sysctl(request, arraylength(request), proc_list.data(), &length, nullptr, 0) != posix::success_response)
     return posix::error_response;
 
   if(proc_list.size() >= length)
@@ -67,7 +70,7 @@ int proclist(pid_t* list, size_t max_length)
 
   struct kinfo_proc* proc_pos = proc_list.data();
   for(pid_t* pos = list; pos != list + length; ++pos, ++proc_pos)
-    *pos = proc_pos->p_pid;
+    *pos = proc_pos->proc.p_pid;
 
   return posix::success_response;
 }
