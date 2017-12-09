55803423a1ff85456223fa3ae93b21f86b437744
GravisZro <github@adaptivetime.com>
2017-12-09 11:28:55 -0500

    allow for streams to be read/written asynchronously

---
+++ asyncfd.cpp
@@ -0,0 +1,66 @@
+#include "asyncfd.h"
+
+// POSIX
+#include <fcntl.h>
+
+// POSIX++
+#include <cassert>
+
+AsyncFD::AsyncFD(posix::fd_t fd) noexcept
+  : m_fd(fd)
+{
+#if defined(O_NONBLOCK) // POSIX
+  int flags = fcntl(m_fd, F_GETFL, 0);
+  assert(flags != posix::error_response);
+  assert(fcntl(m_fd, F_SETFL, flags | O_NONBLOCK) != posix::error_response);
+#else // pre-POSIX
+  int flags = 1;
+  assert(ioctl(m_fd, FIOBIO, &flags) != posix::error_response);
+#endif
+}
+
+void AsyncFD::read(void* buffer, posix::size_t size) noexcept
+  { async_read(buffer, size, 0, size); }
+
+void AsyncFD::write(const void* buffer, posix::size_t size) noexcept
+  { async_write(buffer, size, 0, size); }
+
+void AsyncFD::async_read(void* buffer, posix::size_t size, posix::off_t offset, posix::size_t remaining) noexcept
+{
+  posix::ssize_t out = posix::ignore_interruption(::pread, m_fd, buffer, remaining, offset);
+  if(out < 0)
+  {
+    size -= remaining;
+    Object::enqueue(readError, m_fd, buffer, size, errno);
+  }
+  else
+  {
+    assert(remaining >= posix::size_t(out));
+    offset += out;
+    remaining -= posix::size_t(out);
+    if(remaining == 0)
+      Object::enqueue(readComplete, m_fd, buffer, size);
+    else
+      Object::singleShot(this, &AsyncFD::async_read, buffer, size, offset, remaining);
+  }
+}
+
+void AsyncFD::async_write(const void* buffer, posix::size_t size, posix::off_t offset, posix::size_t remaining) noexcept
+{
+  posix::ssize_t out = posix::ignore_interruption(::pwrite, m_fd, buffer, remaining, offset);
+  if(out < 0)
+  {
+    size -= remaining;
+    Object::enqueue(writeError, m_fd, size, errno);
+  }
+  else
+  {
+    assert(remaining >= posix::size_t(out));
+    offset += out;
+    remaining -= posix::size_t(out);
+    if(remaining == 0)
+      Object::enqueue(writeComplete, m_fd, size);
+    else
+      Object::singleShot(this, &AsyncFD::async_write, buffer, size, offset, remaining);
+  }
+}
---
+++ asyncfd.h
@@ -0,0 +1,27 @@
+#ifndef ASYNCFD_H
+#define ASYNCFD_H
+
+// PDTK
+#include <object.h>
+#include <cxxutils/posix_helpers.h>
+
+class AsyncFD : public Object
+{
+public:
+  AsyncFD(posix::fd_t fd) noexcept;
+
+  void read (      void* buffer, posix::size_t size) noexcept;
+  void write(const void* buffer, posix::size_t size) noexcept;
+
+  signal<posix::fd_t, void*, posix::size_t> readComplete;
+  signal<posix::fd_t, void*, posix::size_t, posix::error_t> readError;
+
+  signal<posix::fd_t, posix::size_t> writeComplete;
+  signal<posix::fd_t, posix::size_t, posix::error_t> writeError;
+private:
+  void async_read (      void* buffer, posix::size_t size, posix::off_t offset, posix::size_t remaining) noexcept;
+  void async_write(const void* buffer, posix::size_t size, posix::off_t offset, posix::size_t remaining) noexcept;
+  posix::fd_t m_fd;
+};
+
+#endif // ASYNCFD_H
