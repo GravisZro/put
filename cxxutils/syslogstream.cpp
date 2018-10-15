b83d5b1bce02c834451360146c0d17b6cb4bfcdd
GravisZro <github@adaptivetime.com>
2018-10-15 14:40:49 -0400

    don't inline _everything_

---
+++ cxxutils/syslogstream.cpp
@@ -0,0 +1,66 @@
+#include "syslogstream.h"
+
+#include <cstring>
+#include <cassert>
+
+namespace posix
+{
+
+  SyslogStream::SyslogStream(void) { clear(); }
+
+  SyslogStream& SyslogStream::operator << (const char* arg) noexcept
+  {
+    constexpr char lookup_table[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                                      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                                      'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                                      'U', 'V', 'W', 'X', 'Y', 'Z' };
+    if(!m_argId)
+      std::strncpy(m_buffer, arg, sizeof(m_buffer));
+    else if(m_argId > 0 && size_t(m_argId) < sizeof(lookup_table))
+    {
+
+      ssize_t buffer_remaining = sizeof(m_tmpbuf);
+      const size_t arglen = std::strlen(arg);
+      std::memset(m_tmpbuf, 0, sizeof(m_tmpbuf));
+
+      char seach_token[3] = { '%', '0', '\0' };
+      seach_token[1] = lookup_table[m_argId];
+
+      char* lastpos = m_buffer;
+      for(char* pos = nullptr;
+          (pos = std::strstr(lastpos, seach_token)) != nullptr;
+          lastpos = pos + 2)
+      {
+        ssize_t slice = pos - lastpos;
+        buffer_remaining -= slice;
+        if(buffer_remaining > 0)
+          std::strncat(m_tmpbuf, lastpos, std::size_t(slice));
+
+        buffer_remaining -= arglen;
+        if(buffer_remaining > 0)
+          std::strncat(m_tmpbuf, arg, arglen);
+      }
+      if(buffer_remaining > 0)
+        std::strncat(m_tmpbuf, lastpos, size_t(buffer_remaining));
+      std::strncpy(m_buffer, m_tmpbuf, sizeof(m_buffer));
+    }
+
+    ++m_argId;
+    return *this;
+  }
+
+  SyslogStream& SyslogStream::operator << (control cntl) noexcept
+  {
+    assert(cntl == control::eom);
+    ::syslog(int(m_priority), "%s", m_buffer);
+    clear();
+    return *this;
+  }
+
+  inline void SyslogStream::clear(void)
+  {
+    std::memset(m_buffer, 0, sizeof(m_buffer));
+    m_priority = priority::info;
+    m_argId = 0;
+  }
+}
--- cxxutils/syslogstream.h
+++ cxxutils/syslogstream.h
@@ -2,8 +2,6 @@
 #define SYSLOG_H
 
 // STL
-#include <ostream>
-#include <streambuf>
 #include <string>
 
 // POSIX
@@ -11,33 +9,6 @@
 
 // POSIX++
 #include <climits>
-#include <cstring>
-#include <cassert>
-
-#ifndef LOG_KERN
-#define LOG_KERN      LOG_LOCAL0
-#endif
-#ifndef LOG_MAIL
-#define LOG_MAIL      LOG_LOCAL1
-#endif
-#ifndef LOG_NEWS
-#define LOG_NEWS      LOG_LOCAL2
-#endif
-#ifndef LOG_UUCP
-#define LOG_UUCP      LOG_LOCAL3
-#endif
-#ifndef LOG_DAEMON
-#define LOG_DAEMON    LOG_LOCAL4
-#endif
-#ifndef LOG_AUTH
-#define LOG_AUTH      LOG_LOCAL5
-#endif
-#ifndef LOG_CRON
-#define LOG_CRON      LOG_LOCAL6
-#endif
-#ifndef LOG_LPR
-#define LOG_LPR       LOG_LOCAL7
-#endif
 
 namespace posix
 {
@@ -75,7 +46,7 @@ namespace posix
   class SyslogStream
   {
   public:
-    SyslogStream(void) { clear(); }
+    SyslogStream(void);
     static void open(const char* name, facility f = facility::provider) noexcept
       { ::openlog(name, LOG_PID | LOG_CONS | LOG_NOWAIT, int(f)); }
     static void close(void) noexcept { ::closelog(); }
@@ -88,62 +59,16 @@ namespace posix
     template<typename T>
     inline SyslogStream& operator << (T val) noexcept { return operator << (std::to_string(val).c_str()); }
 
-    inline SyslogStream& operator << (const char* arg) noexcept
-    {
-      if(m_argId == '0')
-        std::strncpy(m_buffer, arg, sizeof(m_buffer));
-      else
-      {
-        ssize_t buffer_remaining = sizeof(m_tmpbuf);
-        const size_t arglen = std::strlen(arg);
-        std::memset(m_tmpbuf, 0, sizeof(m_tmpbuf));
-
-        char seach_token[3] = { '%', '0', '\0' };
-        seach_token[1] = m_argId;
-
-        char* lastpos = m_buffer;
-        for(char* pos = nullptr;
-            (pos = std::strstr(lastpos, seach_token)) != nullptr;
-            lastpos = pos + 2)
-        {
-          ssize_t slice = pos - lastpos;
-          buffer_remaining -= slice;
-          if(buffer_remaining > 0)
-            std::strncat(m_tmpbuf, lastpos, std::size_t(slice));
-
-          buffer_remaining -= arglen;
-          if(buffer_remaining > 0)
-            std::strncat(m_tmpbuf, arg, arglen);
-        }
-        if(buffer_remaining > 0)
-          std::strncat(m_tmpbuf, lastpos, size_t(buffer_remaining));
-        std::strncpy(m_buffer, m_tmpbuf, sizeof(m_buffer));
-      }
-
-      ++m_argId;
-      return *this;
-    }
-
-    SyslogStream& operator << (control cntl) noexcept
-    {
-      assert(cntl == control::eom);
-      ::syslog(int(m_priority), "%s", m_buffer);
-      clear();
-      return *this;
-    }
+    SyslogStream& operator << (const char* arg) noexcept;
+    SyslogStream& operator << (control cntl) noexcept;
 
   private:
-    void clear(void)
-    {
-      std::memset(m_buffer, 0, sizeof(m_buffer));
-      m_priority = priority::info;
-      m_argId = '0';
-    }
+    void clear(void);
 
     priority m_priority;
     char m_buffer[0x1000 + PATH_MAX];
     char m_tmpbuf[sizeof(m_buffer)];
-    char m_argId;
+    uint8_t m_argId;
   };
 
   static SyslogStream syslog;
