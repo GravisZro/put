f8f7aaab40fd0b6e7c0039da6dd9586ea880dd74
GravisZro <github@adaptivetime.com>
2018-11-01 18:11:02 -0400

    moved from buildtest.cpp

---
+++ tests/build.cpp
@@ -0,0 +1,32 @@
+#include <socket.h>
+#include <childprocess.h>
+#include <application.h>
+#include <object.h>
+//#include <integration/sdl.h>
+#include <specialized/proclist.h>
+#include <specialized/FileEvent.h>
+#include <specialized/PollEvent.h>
+#include <specialized/ProcessEvent.h>
+#include <specialized/eventbackend.h>
+#include <specialized/peercred.h>
+#include <specialized/procstat.h>
+#include <cxxutils/pipedspawn.h>
+#include <cxxutils/vfifo.h>
+#include <cxxutils/pipedfork.h>
+#include <cxxutils/cstringarray.h>
+#include <cxxutils/configmanip.h>
+#include <cxxutils/misc_helpers.h>
+#include <cxxutils/posix_helpers.h>
+#include <cxxutils/socket_helpers.h>
+#include <cxxutils/ranged.h>
+#include <cxxutils/error_helpers.h>
+#include <cxxutils/vterm.h>
+#include <cxxutils/sharedmem.h>
+#include <cxxutils/syslogstream.h>
+#include <cxxutils/nullable.h>
+#include <cxxutils/hashing.h>
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}
