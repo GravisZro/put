5367ba04c22a637472abe9ff3441f1a2351f6be0
GravisZro <github@adaptivetime.com>
2018-11-02 00:16:26 -0400

    fill out rest of code.  untested!

--- specialized/peercred.cpp
+++ specialized/peercred.cpp
@@ -7,7 +7,7 @@
 // PDTK
 #include <cxxutils/error_helpers.h>
 
-#if defined(__sun) && defined(__SVR4) // Solaris
+#if defined(__solaris__) // Solaris
 #pragma message("Information: using Solaris code")
 
 #include <ucred.h>
@@ -37,21 +37,23 @@ int send_cred(int) noexcept
 
 #elif defined (SO_PEERCRED) || defined (LOCAL_PEEREID) /* Linux/OpenBSD/Old NetBSD */
 
-# if defined (SO_PEERCRED) && defined(__linux__)
+# if defined (SO_PEERCRED) && \
+    (defined(__linux__)    /* Linux    */ || \
+     defined(__kFreeBSD__) /* kFreeBSD */ )
 constexpr int socket_cred_option = SO_PEERCRED;
 typedef ucred cred_t;
 constexpr pid_t peer_pid(const cred_t& data) { return data.pid; }
 constexpr uid_t peer_uid(const cred_t& data) { return data.uid; }
 constexpr gid_t peer_gid(const cred_t& data) { return data.gid; }
 
-# elif defined (SO_PEERCRED) && defined(__OpenBSD__)
+# elif defined(SO_PEERCRED) && defined(__OpenBSD__)
 constexpr int socket_cred_option = SO_PEERCRED;
 typedef sockpeercred cred_t;
 constexpr pid_t peer_pid(const cred_t& data) { return data.pid; }
 constexpr uid_t peer_uid(const cred_t& data) { return data.uid; }
 constexpr gid_t peer_gid(const cred_t& data) { return data.gid; }
 
-# elif defined (LOCAL_PEEREID) && defined(__NetBSD__)
+# elif defined(LOCAL_PEEREID) && defined(__NetBSD__) /* legacy NetBSD */
 constexpr int socket_cred_option = LOCAL_PEEREID;
 typedef unpcbid cred_t;
 constexpr pid_t peer_pid(const cred_t& data) { return data.unp_pid; }
@@ -85,7 +87,7 @@ int send_cred(int) noexcept
 { return posix::success_response; }
 
 
-#elif defined (SCM_CREDS) && (defined(__APPLE__) && defined(__MACH__)) /* Darwin */
+#elif defined(SCM_CREDS) && defined(__darwin__) /* Darwin */
 
 #pragma message("Warning: recv_cred/send_cred for Darwin is broken!")
 
@@ -95,23 +97,26 @@ int recv_cred(int, proccred_t&) noexcept
 int send_cred(int) noexcept
 { return posix::error_response; }
 
-#elif defined(SCM_CREDENTIALS) || defined (SCM_CREDS)
+#elif defined(SCM_CREDENTIALS) || defined(SCM_CREDS)
 
-# if defined (SCM_CREDENTIALS) && defined(__linux__)
+# if defined(SCM_CREDENTIALS) /* Linux / kFreeBSD */
 constexpr int credential_message = SCM_CREDENTIALS;
 typedef ucred cred_t;
 constexpr pid_t peer_pid(const cred_t& data) { return data.pid; }
 constexpr uid_t peer_uid(const cred_t& data) { return data.uid; }
 constexpr gid_t peer_gid(const cred_t& data) { return data.gid; }
 
-# elif defined (SCM_CREDS) && defined(__NetBSD__)
+# elif defined(SCM_CREDS) && defined(__NetBSD__) /* NetBSD */
 constexpr int credential_message = SCM_CREDS;
 typedef cred cred_t;
 constexpr pid_t peer_pid(const cred_t& data) { return data.sc_pid; }
 constexpr uid_t peer_uid(const cred_t& data) { return data.sc_euid; }
 constexpr gid_t peer_gid(const cred_t& data) { return data.sc_egid; }
 
-# elif defined (SCM_CREDS) && defined(__FreeBSD__) /* FreeBSD */
+# elif defined(SCM_CREDS) && \
+      (defined(__FreeBSD__)   /* FreeBSD      */ || \
+       defined(__DragonFly__) /* DragonFlyBSD */ || \
+       defined(__gnu_hurd__)  /* GNU/HURD     */ )
 constexpr int credential_message = SCM_CREDS;
 typedef cmsgcred cred_t;
 constexpr pid_t peer_pid(const cred_t& data) { return data.cmcred_pid; }
@@ -125,45 +130,74 @@ constexpr gid_t peer_gid(const cred_t& data) { return data.cmcred_groups[0]; }
 
 int recv_cred(int socket, proccred_t& cred) noexcept
 {
+  struct msghdr message;
+  union
+  {
+    char    rawbuffer[CMSG_SPACE(sizeof(cred_t))];
+    cmsghdr formatted;
+  } cmsg_header;
 
-  return posix::success_response;
+  static_assert(sizeof(cmsg_header.rawbuffer) == CMSG_SPACE(sizeof(cred_t)), "sizeof() is acting improperly");
+  std::memset(cmsg_header.rawbuffer, 0, sizeof(cmsg_header.rawbuffer)); // initialize memory
+
+  char data = 0; // dummy data
+  struct iovec iov = { &data, sizeof(data) };
+
+  message.msg_iov = &iov;
+  message.msg_iovlen = 1;
+  message.msg_name = NULL;
+  message.msg_namelen = 0;
+  message.msg_control = cmsg_header.formatted;
+  message.msg_controllen = sizeof(cmsg_header.rawbuffer);
+  message.msg_flags = 0;
+
+  int nr = ::recvmsg(socket, &message, 0);
+  if(nr == posix::error_response)
+    return posix::error_response;
+
+  if(CMSG_FIRSTHDR(&message) == &cmsg_header.formatted &&
+     cmsg_header.formatted.cmsg_len   <= sizeof(cmsg_header.rawbuffer) &&
+     cmsg_header.formatted.cmsg_level == SOL_SOCKET &&
+     cmsg_header.formatted.cmsg_type  == credential_message)
+  {
+    const cred_t& data = *CMSG_DATA(&cmsg_header.formatted);
+    cred.pid = peer_pid(data);
+    cred.uid = peer_uid(data);
+    cred.gid = peer_gid(data);
+    return posix::success_response;
+  }
+  return posix::error_response;
 }
 
 
 int send_cred(int socket) noexcept
 {
-  struct sockaddr_storage sockaddr;
-  struct sockaddr_un* addr;
-  struct msghdr msg;
-  struct cmsghdr* cmsg_header;
-  struct iovec iov[1];
-  ssize_t nbytes;
-  int i;
-  char buf[CMSG_SPACE(sizeof(cred_t))], c;
+  struct msghdr message;
+  union
+  {
+    char    rawbuffer[CMSG_SPACE(sizeof(cred_t))];
+    cmsghdr formatted;
+  } cmsg_header;
 
-  addr = (struct sockaddr_un *)&sockaddr;
-  addr->sun_family = AF_LOCAL;
-  strlcpy(addr->sun_path, sockpath, sizeof(addr->sun_path));
-  addr->sun_len = SUN_LEN(addr);
+  static_assert(sizeof(cmsg_header.rawbuffer) == CMSG_SPACE(sizeof(cred_t)), "sizeof() is acting improperly");
+  std::memset(cmsg_header.rawbuffer, 0, sizeof(cmsg_header.rawbuffer)); // initialize memory
 
-  c = '*';
-  iov[0].iov_base = &c;
-  iov[0].iov_len = sizeof(c);
-  memset(buf, 0x0b, sizeof(buf));
-  cmsg_header = (struct cmsghdr *)buf;
-  cmsg_header->cmsg_len = CMSG_LEN(sizeof(cred_t));
-  cmsg_header->cmsg_level = SOL_SOCKET;
-  cmsg_header->cmsg_type = credential_message;
+  cmsg_header.formatted.cmsg_len   = CMSG_LEN(sizeof(cred_t));
+  cmsg_header.formatted.cmsg_level = SOL_SOCKET;
+  cmsg_header.formatted.cmsg_type  = credential_message;
 
-  msg.msg_name = NULL;
-  msg.msg_namelen = 0;
-  msg.msg_iov = iov;
-  msg.msg_iovlen = sizeof(iov) / sizeof(iov[0]);
-  msg.msg_control = cmsg_header;
-  msg.msg_controllen = CMSG_SPACE(sizeof(cred_t));
-  msg.msg_flags = 0;
+  char data = 0; // dummy data
+  struct iovec iov = { &data, sizeof(data) };
 
-  return ::sendmsg(fd, &msg, 0);
+  message.msg_iov = &iov;
+  message.msg_iovlen = 1;
+  message.msg_name = NULL;
+  message.msg_namelen = 0;
+  message.msg_control = cmsg_header.formatted;
+  message.msg_controllen = sizeof(cmsg_header.rawbuffer);
+  message.msg_flags = 0;
+
+  return ::sendmsg(socket, &message, 0);
 }
 
 
