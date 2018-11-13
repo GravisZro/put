7be77d688157fac45817c850d6a3fc375bbec9b7
GravisZro <github@adaptivetime.com>
2018-11-13 12:24:02 -0500

    change to boolean

--- specialized/mount.h
+++ specialized/mount.h
@@ -1,11 +1,11 @@
 #ifndef MOUNT_H
 #define MOUNT_H
 
-int mount(const char* device,
+bool mount(const char* device,
           const char* path,
           const char* filesystem,
           const char* options) noexcept;
 
-int unmount(const char* path) noexcept;
+bool unmount(const char* path) noexcept;
 
 #endif // MOUNT_H
