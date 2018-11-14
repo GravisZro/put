244df2a17f62cfafdd723945f7767681836f6606
GravisZro <github@adaptivetime.com>
2018-11-14 01:12:54 -0500

    decode loop rework, allow all options

--- specialized/mount.cpp
+++ specialized/mount.cpp
@@ -38,195 +38,361 @@
 #define MNT_RDONLY        M_RDONLY
 #endif
 
+
+// Linux Only
+#ifndef MS_NODIRATIME
+# define MS_NODIRATIME 0
+#endif
+#ifndef MS_MOVE
+# define MS_MOVE 0
+#endif
+#ifndef MS_BIND
+# define MS_BIND 0
+#endif
+#ifndef MS_REMOUNT
+# define MS_REMOUNT 0
+#endif
+#ifndef MS_MANDLOCK
+# define MS_MANDLOCK 0
+#endif
+#ifndef MS_DIRSYNC
+# define MS_DIRSYNC 0
+#endif
+#ifndef MS_REC
+# define MS_REC 0
+#endif
+#ifndef MS_SILENT
+# define MS_SILENT 0
+#endif
+#ifndef MS_POSIXACL
+# define MS_POSIXACL 0
+#endif
+#ifndef MS_UNBINDABLE
+# define MS_UNBINDABLE 0
+#endif
+#ifndef MS_PRIVATE
+# define MS_PRIVATE 0
+#endif
+#ifndef MS_SLAVE
+# define MS_SLAVE 0
+#endif
+#ifndef MS_SHARED
+# define MS_SHARED 0
+#endif
+#ifndef MS_KERNMOUNT
+# define MS_KERNMOUNT 0
+#endif
+#ifndef MS_I_VERSION
+# define MS_I_VERSION 0
+#endif
+#ifndef MS_STRICTATIME
+# define MS_STRICTATIME 0
+#endif
+#ifndef MS_LAZYTIME
+# define MS_LAZYTIME 0
+#endif
+#ifndef MS_ACTIVE
+# define MS_ACTIVE 0
+#endif
+#ifndef MS_NOUSER
+# define MS_NOUSER 0
+#endif
+
+#ifndef MNT_UNION
+# define MNT_UNION 0
+#endif
+#ifndef MNT_HIDDEN
+# define MNT_HIDDEN 0
+#endif
+#ifndef MNT_NOCOREDUMP
+# define MNT_NOCOREDUMP 0
+#endif
+#ifndef MNT_NOATIME
+# define MNT_NOATIME 0
+#endif
+#ifndef MNT_RELATIME
+# define MNT_RELATIME 0
+#endif
+#ifndef MNT_NODEVMTIME
+# define MNT_NODEVMTIME 0
+#endif
+#ifndef MNT_SYMPERM
+# define MNT_SYMPERM 0
+#endif
+#ifndef MNT_ASYNC
+# define MNT_ASYNC 0
+#endif
+#ifndef MNT_LOG
+# define MNT_LOG 0
+#endif
+#ifndef MNT_EXTATTR
+# define MNT_EXTATTR 0
+#endif
+#ifndef MNT_SNAPSHOT
+# define MNT_SNAPSHOT 0
+#endif
+#ifndef MNT_SUIDDIR
+# define MNT_SUIDDIR 0
+#endif
+#ifndef MNT_NOCLUSTERR
+# define MNT_NOCLUSTERR 0
+#endif
+#ifndef MNT_NOCLUSTERW
+# define MNT_NOCLUSTERW 0
+#endif
+#ifndef MNT_EXTATTR
+# define MNT_EXTATTR 0
+#endif
+#ifndef MNT_SOFTDEP
+# define MNT_SOFTDEP 0
+#endif
+#ifndef MNT_WXALLOWED
+# define MNT_WXALLOWED 0
+#endif
+
+#ifndef MNT_FORCE
+# define MNT_FORCE 0
+#endif
+#ifndef MNT_DETACH
+# define MNT_DETACH 0
+#endif
+#ifndef MNT_DETACH
+# define MNT_EXPIRE 0
+#endif
+#ifndef UMOUNT_NOFOLLOW
+# define UMOUNT_NOFOLLOW 0
+#endif
+
+constexpr posix::size_t section_length(const char* start, const char* end)
+  { return end == NULL ? std::strlen(start) : posix::size_t(end - start); }
+
 bool mount(const char* device,
            const char* path,
            const char* filesystem,
            const char* options) noexcept
 {
-  unsigned long mountflags = 0;
   std::list<std::string> fslist;
+  char *pos, *next;
 #if defined(__linux__)
   std::string optionlist;
-#endif
-
-  for(const char* pos = filesystem, *next = NULL; pos != NULL; pos = next)
-  {
-    if(*pos == ',')
-      ++pos;
-    next = std::strchr(pos, ',');
-
-    if(next == NULL) // end of string
-      fslist.emplace_back(pos);
-    else // not end of string
-      fslist.emplace_back(pos, posix::size_t(next - pos));
-  }
-
-  for(const char* pos = options, *next = NULL; pos != NULL; pos = next)
-  {
-    if(*pos == ',')
-      ++pos;
-    std::string option;
-    next = std::strchr(pos, ',');
-    if(next == NULL) // end of string
-      option.assign(pos);
-    else // not end of string
-      option.assign(pos, posix::size_t(next - pos));
-
-    switch(hash(option))
-    {
-      case "defaults"_hash: break;
-
-// Generic
-#ifdef MNT_RDONLY
-      case "ro"_hash          : mountflags |= MNT_RDONLY      ; break;
-      case "rw"_hash          : mountflags &= 0^MNT_RDONLY    ; break;
-#endif
-#ifdef MNT_NOEXEC
-      case "noexec"_hash      : mountflags |= MNT_NOEXEC      ; break;
-#endif
-#ifdef MNT_NOSUID
-      case "nosuid"_hash      : mountflags |= MNT_NOSUID      ; break;
-#endif
-#ifdef MNT_NODEV
-      case "nodev"_hash       : mountflags |= MNT_NODEV       ; break;
-#endif
-#ifdef MNT_SYNCHRONOUS
-      case "sync"_hash        : mountflags |= MNT_SYNCHRONOUS ; break;
-#endif
-
-// Linux Only
-#ifdef MS_NODIRATIME
-      case "nodiratime"_hash  : mountflags |= MS_NODIRATIME   ; break;
-#endif
-#ifdef MS_MOVE
-      case "move"_hash        : mountflags |= MS_MOVE         ; break;
-#endif
-#ifdef MS_BIND
-      case "bind"_hash        : mountflags |= MS_BIND         ; break;
-#endif
-#ifdef MS_REMOUNT
-      case "remount"_hash     : mountflags |= MS_REMOUNT      ; break;
-#endif
-#ifdef MS_MANDLOCK
-      case "mandlock"_hash    : mountflags |= MS_MANDLOCK     ; break;
-#endif
-#ifdef MS_DIRSYNC
-      case "dirsync"_hash     : mountflags |= MS_DIRSYNC      ; break;
-#endif
-#ifdef MS_REC
-      case "rec"_hash         : mountflags |= MS_REC          ; break;
-#endif
-#ifdef MS_SILENT
-      case "silent"_hash      : mountflags |= MS_SILENT       ; break;
-#endif
-#ifdef MS_POSIXACL
-      case "posixacl"_hash    : mountflags |= MS_POSIXACL     ; break;
-#endif
-#ifdef MS_UNBINDABLE
-      case "unbindable"_hash  : mountflags |= MS_UNBINDABLE   ; break;
-#endif
-#ifdef MS_PRIVATE
-      case "private"_hash     : mountflags |= MS_PRIVATE      ; break;
-#endif
-#ifdef MS_SLAVE
-      case "slave"_hash       : mountflags |= MS_SLAVE        ; break;
-#endif
-#ifdef MS_SHARED
-      case "shared"_hash      : mountflags |= MS_SHARED       ; break;
-#endif
-#ifdef MS_KERNMOUNT
-      case "kernmount"_hash   : mountflags |= MS_KERNMOUNT    ; break;
-#endif
-#ifdef MS_I_VERSION
-      case "iversion"_hash    : mountflags |= MS_I_VERSION    ; break;
-#endif
-#ifdef MS_STRICTATIME
-      case "strictatime"_hash : mountflags |= MS_STRICTATIME  ; break;
-#endif
-#ifdef MS_LAZYTIME
-      case "lazytime"_hash    : mountflags |= MS_LAZYTIME     ; break;
-#endif
-#ifdef MS_ACTIVE
-      case "active"_hash      : mountflags |= MS_ACTIVE       ; break;
-#endif
-#ifdef MS_NOUSER
-      case "nouser"_hash      : mountflags |= MS_NOUSER       ; break;
-#endif
-
-// BSDs
-#ifdef MNT_UNION
-      case "union"_hash       : mountflags |= MNT_UNION       ; break;
-#endif
-#ifdef MNT_HIDDEN
-      case "hidden"_hash      : mountflags |= MNT_HIDDEN      ; break;
-#endif
-#ifdef MNT_NOCOREDUMP
-      case "nocoredump"_hash  : mountflags |= MNT_NOCOREDUMP  ; break;
-#endif
-#ifdef MNT_NOATIME
-      case "noatime"_hash     : mountflags |= MNT_NOATIME     ; break;
-#endif
-#ifdef MNT_RELATIME
-      case "relatime"_hash    : mountflags |= MNT_RELATIME    ; break;
-      case "norelatime"_hash  : mountflags &= 0^MNT_RELATIME  ; break;
-#endif
-#ifdef MNT_NODEVMTIME
-      case "nodevmtime"_hash  : mountflags |= MNT_NODEVMTIME  ; break;
-#endif
-#ifdef MNT_SYMPERM
-      case "symperm"_hash     : mountflags |= MNT_SYMPERM     ; break;
-#endif
-#ifdef MNT_ASYNC
-      case "async"_hash       : mountflags |= MNT_ASYNC       ; break;
-#endif
-#ifdef MNT_LOG
-      case "log"_hash         : mountflags |= MNT_LOG         ; break;
-#endif
-#ifdef MNT_EXTATTR
-      case "extattr"_hash     : mountflags |= MNT_EXTATTR     ; break;
-#endif
-#ifdef MNT_SNAPSHOT
-      case "snapshot"_hash    : mountflags |= MNT_SNAPSHOT    ; break;
-#endif
-#ifdef MNT_SUIDDIR
-      case "suiddir"_hash     : mountflags |= MNT_SUIDDIR     ; break;
-#endif
-#ifdef MNT_FORCE
-      case "force"_hash       : mountflags |= MNT_FORCE       ; break;
-#endif
-#ifdef MNT_NOCLUSTERR
-      case "noclusterr"_hash  : mountflags |= MNT_NOCLUSTERR  ; break;
-#endif
-#ifdef MNT_NOCLUSTERW
-      case "noclusterw"_hash  : mountflags |= MNT_NOCLUSTERW  ; break;
-#endif
-#ifdef MNT_EXTATTR
-      case "extattr"_hash     : mountflags |= MNT_EXTATTR     ; break;
-#endif
-#ifdef MNT_DETACH
-      case "detach"_hash      : mountflags |= MNT_DETACH      ; break;
-#endif
-#ifdef MNT_SOFTDEP
-      case "softdep"_hash     : mountflags |= MNT_SOFTDEP     ; break;
-#endif
-#ifdef MNT_WXALLOWED
-      case "wxallowed"_hash   : mountflags |= MNT_WXALLOWED   ; break;
-#endif
-      default:
-#if defined(__linux__)
-        optionlist.append(option).append(1, ',');
+  unsigned long mountflags = 0;
 #else
+  int mountflags = 0;
 #endif
-        break;
-    }
+
+  pos = const_cast<char*>(filesystem);
+  next = std::strtok(pos, ",");
+  do
+  {
+    fslist.emplace_back(pos, section_length(pos, next));
+    pos = next;
+    next = std::strtok(NULL, ",");
+  } while(pos != NULL && ++pos);
+
+  if(options != nullptr)
+  {
+    pos = const_cast<char*>(options);
+    next = std::strtok(pos, ",");
+    do
+    {
+      switch(hash(pos, section_length(pos, next)))
+      {
+        case "defaults"_hash: break;
+
+  // Generic
+        case "ro"_hash          : mountflags |= MNT_RDONLY      ; break;
+        case "rw"_hash          : mountflags &= 0^MNT_RDONLY    ; break;
+        case "noexec"_hash      : mountflags |= MNT_NOEXEC      ; break;
+        case "nosuid"_hash      : mountflags |= MNT_NOSUID      ; break;
+        case "nodev"_hash       : mountflags |= MNT_NODEV       ; break;
+        case "sync"_hash        : mountflags |= MNT_SYNCHRONOUS ; break;
+        case "nodiratime"_hash  : mountflags |= MS_NODIRATIME   ; break;
+        case "move"_hash        : mountflags |= MS_MOVE         ; break;
+        case "bind"_hash        : mountflags |= MS_BIND         ; break;
+        case "remount"_hash     : mountflags |= MS_REMOUNT      ; break;
+        case "mandlock"_hash    : mountflags |= MS_MANDLOCK     ; break;
+        case "dirsync"_hash     : mountflags |= MS_DIRSYNC      ; break;
+        case "rec"_hash         : mountflags |= MS_REC          ; break;
+        case "silent"_hash      : mountflags |= MS_SILENT       ; break;
+        case "posixacl"_hash    : mountflags |= MS_POSIXACL     ; break;
+        case "unbindable"_hash  : mountflags |= MS_UNBINDABLE   ; break;
+        case "private"_hash     : mountflags |= MS_PRIVATE      ; break;
+        case "slave"_hash       : mountflags |= MS_SLAVE        ; break;
+        case "shared"_hash      : mountflags |= MS_SHARED       ; break;
+        case "kernmount"_hash   : mountflags |= MS_KERNMOUNT    ; break;
+        case "iversion"_hash    : mountflags |= MS_I_VERSION    ; break;
+        case "strictatime"_hash : mountflags |= MS_STRICTATIME  ; break;
+        case "lazytime"_hash    : mountflags |= MS_LAZYTIME     ; break;
+        case "active"_hash      : mountflags |= MS_ACTIVE       ; break;
+        case "nouser"_hash      : mountflags |= MS_NOUSER       ; break;
+        case "union"_hash       : mountflags |= MNT_UNION       ; break;
+        case "hidden"_hash      : mountflags |= MNT_HIDDEN      ; break;
+        case "nocoredump"_hash  : mountflags |= MNT_NOCOREDUMP  ; break;
+        case "noatime"_hash     : mountflags |= MNT_NOATIME     ; break;
+        case "relatime"_hash    : mountflags |= MNT_RELATIME    ; break;
+        case "norelatime"_hash  : mountflags &= 0^MNT_RELATIME  ; break;
+        case "nodevmtime"_hash  : mountflags |= MNT_NODEVMTIME  ; break;
+        case "symperm"_hash     : mountflags |= MNT_SYMPERM     ; break;
+        case "async"_hash       : mountflags |= MNT_ASYNC       ; break;
+        case "log"_hash         : mountflags |= MNT_LOG         ; break;
+        case "extattr"_hash     : mountflags |= MNT_EXTATTR     ; break;
+        case "snapshot"_hash    : mountflags |= MNT_SNAPSHOT    ; break;
+        case "suiddir"_hash     : mountflags |= MNT_SUIDDIR     ; break;
+        case "noclusterr"_hash  : mountflags |= MNT_NOCLUSTERR  ; break;
+        case "noclusterw"_hash  : mountflags |= MNT_NOCLUSTERW  ; break;
+        case "softdep"_hash     : mountflags |= MNT_SOFTDEP     ; break;
+        case "wxallowed"_hash   : mountflags |= MNT_WXALLOWED   ; break;
+        default:
+  #if defined(__linux__)
+          optionlist.append(pos, section_length(pos, next));
+          if(next != NULL)
+              optionlist.append(1, ',');
+  #elif defined(__FreeBSD__) && KERNEL_VERSION_CODE >= KERNEL_VERSION(6,0,0)
+
+  #else
+  // NetBSD
+          MOUNT_FFS
+           struct ufs_args {
+            char	   *fspec;	       /* block	special	file to	mount */
+           };
+
+          MOUNT_NFS
+           struct nfs_args {
+            int		 version;      /* args structure version */
+            struct	sockaddr *addr;	       /* file server address */
+            int		 addrlen;      /* length of address */
+            int		 sotype;       /* Socket type */
+            int		 proto;	       /* and Protocol */
+            u_char		 *fh;	       /* File handle to be mounted */
+            int		 fhsize;       /* Size,	in bytes, of fh	*/
+            int		 flags;	       /* flags	*/
+            int		 wsize;	       /* write	size in	bytes */
+            int		 rsize;	       /* read size in bytes */
+            int		 readdirsize;  /* readdir size in bytes	*/
+            int		 timeo;	       /* initial timeout in .1	secs */
+            int		 retrans;      /* times	to retry send */
+            int		 maxgrouplist; /* Max. size of group list */
+            int		 readahead;    /* # of blocks to readahead */
+            int		 leaseterm;    /* Term (sec) of	lease */
+            int		 deadthresh;   /* Retrans threshold */
+            char		 *hostname;    /* server's name	*/
+           };
+
+          MOUNT_MFS
+           struct mfs_args {
+            char	   *fspec;	       /* name to export for statfs */
+            struct	   export_args30 pad;  /* unused */
+            caddr_t   base;	       /* base of file system in mem */
+            u_long	   size;	       /* size of file system */
+           };
+
+
+
+  // Open BSD
+           MOUNT_CD9660
+            struct iso_args {
+                char	   *fspec;     /* block	special	device to mount	*/
+                struct	   export_args export_info;
+                         /* network export info */
+                int flags;	       /* mounting flags, see below */
+            };
+            #define ISOFSMNT_NORRIP   0x00000001	/* disable Rock	Ridge Ext.*/
+            #define ISOFSMNT_GENS     0x00000002	/* enable generation numbers */
+            #define ISOFSMNT_EXTATT   0x00000004	/* enable extended attributes */
+            #define ISOFSMNT_NOJOLIET 0x00000008	/* disable Joliet Ext.*/
+            #define ISOFSMNT_SESS     0x00000010	/* use iso_args.sess */
+
+           MOUNT_FFS
+            struct ufs_args {
+             char	   *fspec;	       /* block	special	file to	mount */
+             struct	   export_args export_info;
+                            /* network export information */
+            };
+
+           MOUNT_MFS
+            struct mfs_args {
+             char	   *fspec;	       /* name to export for statfs */
+             struct	   export_args export_info;
+                            /* if we	can export an MFS */
+             caddr_t   base;	       /* base of filesystem in	mem */
+             u_long	   size;	       /* size of filesystem */
+            };
+
+           MOUNT_MSDOS
+            struct msdosfs_args {
+               char	   *fspec;    /* blocks	special	holding	fs to mount */
+               struct  export_args export_info;
+                        /* network export	information */
+               uid_t   uid;	      /* uid that owns msdosfs files */
+               gid_t   gid;	      /* gid that owns msdosfs files */
+               mode_t  mask;      /* mask to be applied for	msdosfs	perms */
+               int	   flags;     /* see below */
+            };
+
+            /*
+             * Msdosfs mount options:
+             */
+            #define MSDOSFSMNT_SHORTNAME	1  /* Force old	DOS short names	only */
+            #define MSDOSFSMNT_LONGNAME	2  /* Force Win'95 long	names */
+            #define MSDOSFSMNT_NOWIN95	4  /* Completely ignore	Win95 entries */
+
+           MOUNT_NFS
+            struct nfs_args {
+             int	   version;	   /* args structure version */
+             struct	sockaddr *addr;	   /* file server address */
+             int	   addrlen;	   /* length of	address	*/
+             int	   sotype;	   /* Socket type */
+             int	   proto;	   /* and Protocol */
+             u_char	   *fh;		   /* File handle to be	mounted	*/
+             int	   fhsize;	   /* Size, in bytes, of fh */
+             int	   flags;	   /* flags */
+             int	   wsize;	   /* write size in bytes */
+             int	   rsize;	   /* read size	in bytes */
+             int	   readdirsize;	   /* readdir size in bytes */
+             int	   timeo;	   /* initial timeout in .1 secs */
+             int	   retrans;	   /* times to retry send */
+             int	   maxgrouplist;   /* Max. size	of group list */
+             int	   readahead;	   /* #	of blocks to readahead */
+             int	   leaseterm;	   /* Term (sec) of lease */
+             int	   deadthresh;	   /* Retrans threshold	*/
+             char	   *hostname;	   /* server's name */
+             int	   acregmin;	 /* Attr cache file recently modified */
+             int	   acregmax;	   /* ac file not recently modified */
+             int	   acdirmin;	   /* ac for dir recently modified */
+             int	   acdirmax;	 /* ac for dir not recently modified */
+            };
+
+           MOUNT_NTFS
+            struct ntfs_args {
+               char	   *fspec; /* block special device to mount */
+               struct  export_args export_info;
+                     /* network export information */
+               uid_t   uid;	   /* uid that owns ntfs files */
+               gid_t   gid;	   /* gid that owns ntfs files */
+               mode_t  mode;   /* mask to be applied for ntfs perms	*/
+               u_long  flag;   /* additional flags */
+            };
+
+            /*
+             * ntfs mount options:
+             */
+            #define     NTFS_MFLAG_CASEINS      0x00000001
+            #define     NTFS_MFLAG_ALLNAMES     0x00000002
+
+           MOUNT_UDF
+            struct udf_args {
+               char	   *fspec; /* block special device to mount */
+            };
+
+  #endif
+          break;
+      }
+      pos = next;
+      next = std::strtok(NULL, ",");
+    } while(pos != NULL && ++pos);
   }
 
   //MNT_UPDATE, MNT_RELOAD, and MNT_GETARGS
-
-#if defined(__linux__)
-  if(optionlist.back() == ',') // find trailing ','
-    optionlist.pop_back(); // remove trailing ','
-#endif
 
   for(const std::string& fs : fslist)
   {
@@ -235,17 +401,45 @@ bool mount(const char* device,
       return true;
 #else
 
+# if defined(__NetBSD__) && KERNEL_VERSION_CODE < KERNEL_VERSION(5,0,0)
+#else
+#endif
+
 #endif
   }
 
   return false;
 }
 
-bool unmount(const char* path) noexcept
+bool unmount(const char* path, const char* options) noexcept
 {
-#if defined(__linux__)
+#if defined(__linux__) && KERNEL_VERSION_CODE < KERNEL_VERSION(2,1,116)
   return umount(path) != posix::success_response;
 #else
-  return unmount(path, 0);
+  int flags = 0;
+
+  if(options != nullptr)
+  {
+    char* pos = const_cast<char*>(options);
+    char* next = std::strtok(pos, ",");
+    do
+    {
+      switch(hash(pos, section_length(pos, next)))
+      {
+        case "detach"_hash  : flags |= MNT_DETACH      ; break;
+        case "force"_hash   : flags |= MNT_FORCE       ; break;
+        case "expire"_hash  : flags |= MNT_EXPIRE      ; break;
+        case "nofollow"_hash: flags |= UMOUNT_NOFOLLOW ; break;
+      }
+      pos = next;
+      next = std::strtok(NULL, ",");
+    } while(pos != NULL && ++pos);
+  }
+
+# if defined(__linux__)
+  return umount2(path, flags) != posix::success_response;
+# else
+  return unmount(path, flags) != posix::success_response;
+# endif
 #endif
 }
