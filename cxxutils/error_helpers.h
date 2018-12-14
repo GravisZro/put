68763a37a72748a6398f08abe6ae54db8f9ab2bf
GravisZro <github@adaptivetime.com>
2018-12-14 17:08:57 -0500

    steal error code enum from STL

--- cxxutils/error_helpers.h
+++ cxxutils/error_helpers.h
@@ -19,32 +19,33 @@
     return rvalue; \
   }
 
-template<typename T>
-constexpr bool operator ==(std::errc err, T err_num) noexcept
-  { return *reinterpret_cast<T*>(&err) == err_num; }
-
-template<typename T>
-constexpr bool operator !=(std::errc err, T err_num) noexcept
-  { return *reinterpret_cast<T*>(&err) != err_num; }
-
-template<typename T>
-constexpr bool operator ==(T err_num, std::errc err) noexcept
-  { return T(err) == err_num; }
-
-template<typename T>
-constexpr bool operator !=(T err_num, std::errc err) noexcept
-  { return T(err) != err_num; }
-
 namespace posix
 {
+  using std::errc;
   typedef int error_t;
   constexpr error_t success_response = 0;
   constexpr error_t error_response = -1;
   static inline bool is_success(void) { return errno == success_response; }
   static inline bool success(void) noexcept { errno = success_response; return true; }
   static inline bool error(error_t err) noexcept { errno = err; return is_success(); }
-  static inline bool error(std::errc err) noexcept { return error(error_t(err)); }
+  static inline bool error(errc err) noexcept { return error(error_t(err)); }
   template<typename T> static inline error_t errorval(T err) noexcept { return error(err) ? posix::success_response : posix::error_response; }
+
+  template<typename T>
+  constexpr bool operator ==(errc err, T err_num) noexcept
+    { return *reinterpret_cast<T*>(&err) == err_num; }
+
+  template<typename T>
+  constexpr bool operator !=(errc err, T err_num) noexcept
+    { return *reinterpret_cast<T*>(&err) != err_num; }
+
+  template<typename T>
+  constexpr bool operator ==(T err_num, errc err) noexcept
+    { return T(err) == err_num; }
+
+  template<typename T>
+  constexpr bool operator !=(T err_num, errc err) noexcept
+    { return T(err) != err_num; }
 }
 
 #endif // ERROR_HELPERS_H
