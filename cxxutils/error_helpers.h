1058c537e80d24d24b8f2be368df36c158e17b3a
GravisZro <github@adaptivetime.com>
2018-12-14 17:29:25 -0500

    opps! :P

--- cxxutils/error_helpers.h
+++ cxxutils/error_helpers.h
@@ -30,23 +30,23 @@ namespace posix
   static inline bool error(error_t err) noexcept { errno = err; return is_success(); }
   static inline bool error(errc err) noexcept { return error(error_t(err)); }
   template<typename T> static inline error_t errorval(T err) noexcept { return error(err) ? posix::success_response : posix::error_response; }
-
-  template<typename T>
-  constexpr bool operator ==(errc err, T err_num) noexcept
-    { return *reinterpret_cast<T*>(&err) == err_num; }
-
-  template<typename T>
-  constexpr bool operator !=(errc err, T err_num) noexcept
-    { return *reinterpret_cast<T*>(&err) != err_num; }
-
-  template<typename T>
-  constexpr bool operator ==(T err_num, errc err) noexcept
-    { return T(err) == err_num; }
-
-  template<typename T>
-  constexpr bool operator !=(T err_num, errc err) noexcept
-    { return T(err) != err_num; }
 }
+
+template<typename T>
+constexpr bool operator ==(posix::errc err, T err_num) noexcept
+  { return *reinterpret_cast<T*>(&err) == err_num; }
+
+template<typename T>
+constexpr bool operator !=(posix::errc err, T err_num) noexcept
+  { return *reinterpret_cast<T*>(&err) != err_num; }
+
+template<typename T>
+constexpr bool operator ==(T err_num, posix::errc err) noexcept
+  { return T(err) == err_num; }
+
+template<typename T>
+constexpr bool operator !=(T err_num, posix::errc err) noexcept
+  { return T(err) != err_num; }
 
 #endif // ERROR_HELPERS_H
 
