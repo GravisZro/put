7ac18b001a9ecc1629bed8874c890d23459b35e8
GravisZro <github@adaptivetime.com>
2019-04-02 12:01:30 -0400

    exclude apple from speed hack

--- cxxutils/hashing.h
+++ cxxutils/hashing.h
@@ -66,7 +66,7 @@ static constexpr uint32_t crc_table[256] = {
 };
 
 constexpr uint32_t crc32_compiletime(const char* str, posix::size_t idx) noexcept
-#if defined(__CONTINUOUS_INTEGRATION__) && defined(__clang__)
+#if defined(__CONTINUOUS_INTEGRATION__) && defined(__clang__) && !defined(__APPLE__)
 // Fast and unreliable hashing
   { return idx ? (static_cast<uint32_t>(str[idx]) + 33 * crc32_compiletime(str, idx - 1)) ^ crc_table[idx + str[idx]] : 5381; }
 #else
@@ -79,7 +79,7 @@ constexpr uint32_t operator "" _hash(const char* str, const posix::size_t sz) no
 
 // runtime hashing
 static inline uint32_t crc32_runtime(const char* str, posix::size_t idx) noexcept
-#if defined(__CONTINUOUS_INTEGRATION__) && defined(__clang__)
+#if defined(__CONTINUOUS_INTEGRATION__) && defined(__clang__) && !defined(__APPLE__)
 // Fast and unreliable hashing
   { return idx ? (static_cast<uint32_t>(str[idx]) + 33 * crc32_runtime(str, idx - 1)) ^ crc_table[idx + str[idx]] : 5381; }
 #else
