96f93f69613d4a4f6ea3e328aecef251c8b2c10c
GravisZro <github@adaptivetime.com>
2021-01-01 18:31:59 -0500

    rewrite

--- cxxutils/vfifo.cpp
+++ cxxutils/vfifo.cpp
@@ -1,86 +1,22 @@
 #include "vfifo.h"
 
-vfifo& vfifo::operator=(const vfifo& other) noexcept
-{
-  if (this != &other)
-  {
-    m_data       = const_cast<vfifo&>(other).m_data;
-    m_virt_begin = other.m_virt_begin;
-    m_virt_end   = other.m_virt_end;
-    m_capacity   = other.m_capacity;
-    m_ok         = other.m_ok;
-    m_external   = other.m_external;
-  }
-  return *this;
-}
-
 bool vfifo::allocate(posix::ssize_t length) noexcept
 {
-  if(m_external)
-    return m_ok = false;
+  if(discarded() && size())
+    posix::memmove(m_data, data(), size());
 
-  if(length <= capacity()) // reducing the allocated size is not allowed!
-    return false;
-  char* new_data = static_cast<char*>(posix::malloc(posix::size_t(length)));
-  if(new_data == nullptr)
-    return false;
-
-  posix::memset(new_data, 0, posix::size_t(length));
-  if(m_data == nullptr) // if no memory allocated yet
+  if(length > capacity())
   {
-    m_data = new_data; // assign memory
-    resize(0); // reset all the progress pointers status
+    m_data = posix::realloc(m_data, length);
+    m_capacity = length;
   }
-  else // expand memory
-  {
-    posix::memcpy(new_data, m_data, posix::size_t(capacity())); // copy old memory to new
-    m_virt_begin = new_data + (m_virt_begin - m_data); // shift the progress pointers to the new memory
-    m_virt_end   = new_data + (m_virt_end   - m_data);
-    delete[] m_data; // delete the old memory
-    m_data = new_data; // use new memory
-  }
-
-  m_capacity = length;
-  m_ok &= m_data != nullptr;
-  return m_data != nullptr;
+  return m_data != NULL;
 }
 
 void vfifo::reset(void) noexcept
 {
-  m_virt_end = m_virt_begin = m_data;
+  m_virt_end = m_virt_begin = 0;
   clearError();
-}
-
-bool vfifo::resize(posix::ssize_t sz) noexcept
-{
-  if(sz < 0)
-    return m_ok = false;
-  m_virt_begin = m_data;
-  m_virt_end   = m_data + sz;
-  m_ok &= m_virt_end <= end();
-  return m_virt_end <= end();
-}
-
-bool vfifo::shrink(posix::ssize_t count) noexcept
-{
-  if(count < 0)
-    return m_ok = false;
-  if(m_virt_begin + count < m_virt_end)
-    m_virt_begin += count;
-  else
-    m_virt_begin = m_virt_end = m_data; // move back to start of buffer
-  return true;
-}
-
-bool vfifo::expand(posix::ssize_t count) noexcept
-{
-  if(count < 0)
-    return m_ok = false;
-  if(m_virt_end + count < end())
-    m_virt_end += count;
-  else
-    m_virt_end = const_cast<char*>(end());
-  return true;
 }
 
 // make sure strings are read in properly!
--- cxxutils/vfifo.h
+++ cxxutils/vfifo.h
@@ -19,26 +19,20 @@ public:
   template<typename... ArgTypes>
   vfifo(ArgTypes&... args) noexcept : vfifo() { serialize(args...); }
 
-  vfifo(char* data, posix::ssize_t length) noexcept
-    : m_data(data), m_capacity(length), m_external(true) { reset(); }
-
   vfifo(posix::ssize_t length = 0x0000FFFF) noexcept  // default size is 64 KiB
-    : m_data(nullptr), m_capacity(0), m_external(false) { allocate(length); }
+    : m_data(nullptr), m_capacity(0) { reset(); allocate(length); }
 
-  vfifo(const vfifo& that) noexcept { operator=(that); }
-
-  vfifo& operator=(const vfifo& other) noexcept;
+  vfifo(vfifo&& that) noexcept = default;
+  vfifo& operator=(vfifo&& other) noexcept = default;
 
 // === error functions ===
-  bool hadError(void) const noexcept { return !m_ok; }
-  void clearError(void) noexcept { m_ok = true; }
+  constexpr bool hadError(void) const noexcept { return !m_ok; }
+  constexpr void clearError(void) noexcept { m_ok = true; }
 
 // === serializer frontends ===
   template<typename T>
   vfifo& operator << (const T& arg) noexcept
   {
-    if(posix::ssize_t(sizeof(T)) > unused())
-      allocate(capacity() * 2);
     serialize(arg);
     return *this;
   }
@@ -53,8 +47,6 @@ public:
   template<typename T, typename... ArgTypes>
   void serialize(const T& arg, ArgTypes&... args) noexcept
   {
-    if(posix::ssize_t(sizeof(T)) > unused())
-      allocate(capacity() * 2);
     serialize(arg);
     serialize(args...);
   }
@@ -67,29 +59,27 @@ public:
   }
 
 // === manual queue manipulators ===
+
+  constexpr bool           empty    (void) const noexcept { return !size(); }
+  constexpr posix::ssize_t size     (void) const noexcept { return m_virt_end - m_virt_begin; }
+  constexpr posix::ssize_t discarded(void) const noexcept { return m_virt_begin; }
+  constexpr posix::ssize_t used     (void) const noexcept { return m_virt_end; }
+  constexpr posix::ssize_t unused   (void) const noexcept { return m_capacity - m_virt_end; }
+  constexpr posix::ssize_t capacity (void) const noexcept { return m_capacity; }
+
   bool allocate(posix::ssize_t length) noexcept;
-
-  bool           empty (void) const noexcept { return dataEnd() == data   (); }
-  posix::ssize_t size  (void) const noexcept { return dataEnd() -  data   (); }
-  posix::ssize_t used  (void) const noexcept { return data   () -  begin  (); }
-  posix::ssize_t unused(void) const noexcept { return end    () -  dataEnd(); }
-
-
   void reset(void) noexcept;
-  bool resize(posix::ssize_t sz) noexcept;
-  bool shrink(posix::ssize_t count) noexcept;
-  bool expand(posix::ssize_t count) noexcept;
 
   template<typename T = char> constexpr T& front   (void) noexcept       { return *data<T>(); }
   template<typename T = char> constexpr T& back    (void) noexcept       { return *dataEnd<T>(); }
 
-  template<typename T = char> constexpr T* data    (void) const noexcept { return reinterpret_cast<T*>(m_virt_begin); }
-  template<typename T = char> constexpr T* dataEnd (void) const noexcept { return reinterpret_cast<T*>(m_virt_end  ); }
+  template<typename T = char> constexpr T* data    (void) const noexcept { return reinterpret_cast<T*>(static_cast<uint8_t*>(m_data) + m_virt_begin); }
+  template<typename T = char> constexpr T* dataEnd (void) const noexcept { return reinterpret_cast<T*>(static_cast<uint8_t*>(m_data) + m_virt_end  ); }
 
-  template<typename T = char>           T* begin   (void) const noexcept { return reinterpret_cast<T*>(m_data); }
-  template<typename T = char> constexpr T* end     (void) const noexcept { return reinterpret_cast<T*>(m_data + m_capacity); }
+  template<typename T = char> constexpr T* begin   (void) const noexcept { return reinterpret_cast<T*>(m_data); }
+  template<typename T = char> constexpr T* end     (void) const noexcept { return reinterpret_cast<T*>(static_cast<uint8_t*>(m_data) + m_capacity); }
 
-  const posix::ssize_t& capacity(void) const { return m_capacity; }
+  constexpr posix::ssize_t& capacity(void) noexcept { return m_capacity; }
 
 private:
   template<typename T>
@@ -108,17 +98,16 @@ private:
     if(data() + sizeof(T) > dataEnd()) // check if this would underflow buffer
       return m_ok = false; // avoid buffer underflow!
     m_virt_begin += sizeof(T);
-    if(m_virt_begin == m_virt_end) // if buffer is empty
-      m_virt_begin = m_virt_end = m_data; // move back to start of buffer
+    if(empty()) // if buffer is empty
+      reset(); // move back to start of buffer
     return true;
   }
 
-  char* m_data;
-  char* m_virt_begin;
-  char* m_virt_end;
+  void* m_data;
+  posix::ssize_t m_virt_begin;
+  posix::ssize_t m_virt_end;
   posix::ssize_t m_capacity;
   bool m_ok;
-  bool m_external;
 
 // === serializer backends ===
 private:
@@ -130,6 +119,11 @@ private:
   template<typename T>
   void serialize_arr(const T* arg, uint16_t length) noexcept
   {
+    if(size() < discarded())
+      allocate(capacity());
+    if(posix::ssize_t(sizeof(T)) > unused())
+      allocate(capacity() * 2);
+
     if(push<uint16_t>(sizeof(T)) &&
        push<uint16_t>(length))
       for(posix::size_t i = 0; m_ok && i < length; ++i)
